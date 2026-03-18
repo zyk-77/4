f4044b6cbd2c58d0fd4ae2a59e7d2fd09d064e09
Yukun Zhang <ZYK20061016@outlook.com>
2026-03-18 16:58:07 +0800

    ac代码

--- 随机数/22.cpp
+++ 随机数/22.cpp
@@ -1,91 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-int f[6] = { 0,1,2,3,4,5 };
-class Chainnode {
-public:
-	int x;
-	Chainnode* next,*last;
- // Make the constructor accessible
-	Chainnode(int i) { x = i; next = nullptr; last = nullptr; }
-};
-Chainnode* ha, * hb, * hc,*e,*index,*ia,*ib;
-
-void outputa() {
-	ia = ha;
-	while (1) {
-		cout << ia->x << ' ';
-		ia = ia->next;
-		if (ia == ha) break;
-	}
-}
-void outputb() {
-	ib = hb;
-	while (1) {
-		cout << ib->x << ' ';
-		ib = ib->next;
-		if (ib == hb) break;
-	}
-}
-void outputc() {
-	index = hc;
-	while (1) {
-		cout << index->x << ' ';
-		index = index->next;
-		if (index == hc) break;
-	}
-}
-
+int n, f[101][101], h[101][101], a[101][101], b[101][101], an[101]={0}, bn[101] = {0}, sum = 0;
 int main() {
-	for (int i = 0; i < 6; ++i) {
-		e = new Chainnode(f[i]);
-		if (i == 0) hc = e,index=hc;
-		else {
-			index->next = e;
-			index->next->last = index;
-			index = e;
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			cin >> f[i][j]; a[i][f[i][j]] = j;
 		}
 	}
-	index->next = hc;
-	hc->last = index;
-	index = hc;
-	int m = 0;
-	while (1) {                 //split方法
-		if (m % 2) {
-			if (!ha) {
-				ha = index;
-				ia = ha;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			cin >> h[i][j]; b[i][h[i][j]] = j;
+		}
+	}
+	int x = 0;
+	while (sum < n) {
+		x++; 
+		if (x > n) x = 1;
+		if (an[x]>0) continue;
+		for (int i = 1; i <= n; ++i) {
+			if (bn[f[x][i]]==0) {
+				bn[f[x][i]] = x;
+				an[x] = f[x][i];
+				sum++;
+				break;
 			}
 			else {
-				ia->next = index;
-				ia->next->last = ia;
-				ia = ia->next;
+				if (b[f[x][i]][x] < b[f[x][i]][bn[f[x][i]]]) {
+					an[bn[f[x][i]]] = 0;
+					bn[f[x][i]] = x;
+					an[x] = f[x][i];
+					break;
+				}
 			}
 		}
-		else {
-			if (!hb) {
-				hb = index;
-				ib = hb;
-			}
-			else {
-				ib->next = index;
-				ib->next->last = ib;
-				ib = ib->next;
-			}
-		}
-		index = index->next;
-		m++;
-		if (index == hc) break;
-	}                           //split方法
-	ia->next = ha;
-	ha->last = ia;
-	ib->next = hb;
-	hb->last = ib;
-	cout << "a:";
-	outputa();
-	cout << endl;
-	cout << "b:";
-	outputb();
-	cout << endl;
-	cout << "c:";
-	outputc();
+	}
+	for (int i = 1; i <= n; ++i) cout << an[i] << ' ';
+	return 0;
 }
